8ef73e5607344406625897c808ea89158a974c1f
j1nma <j1nma@users.noreply.github.com>
2019-05-19 05:09:48 -0300

    kineticEnergyWithD with optional jump

--- scripts/kineticEnergyWithD.m
+++ scripts/kineticEnergyWithD.m
@@ -1,5 +1,5 @@
-function kineticEnergy
-	fid = fopen('./output/energy_file_D=0.0.txt');
+function kineticEnergyWithD(diameter, dirName)
+	fid = fopen(sprintf("./output/energy_file_D=%d.txt", diameter));
 
 	time = [0.0];
 	energy = [0.0];
@@ -26,19 +26,19 @@ function kineticEnergy
 
 	fclose(fid);
 
-    jump = find(energy(2:end) < 1e-6, 1, 'first');
+    #jump = find(energy(2:end) < 1e-18, 1, 'first');
 
-    props = {"marker", '.', 'LineStyle', 'none'};
-    h = plot(time, log10(energy));
+    props = {'marker', '.', 'LineStyle', 'none'};
+    h = plot(time, log10(energy), sprintf(";D = %d;", diameter));
     set(h, props{:})
-    xlabel("Tiempo (s)");
-    ylabel("log_{10}(Energía cinética) (J)");
-    axis([0 time(end)])
-    set(gca, "xtick", time(jump + 1))
+    xlabel("Tiempo [s]");
+    ylabel("log_{10}(Energía cinética) [J]");
+    #set(gca, "xtick", time(jump + 1))
     grid on
+    legend("location", "southeast");
 
     hold all
 
-	print(sprintf("./output/kineticEnergy-N=%d-T=%ds-Log10.png", N, round(time(end))), "-dpngcairo", "-F:14")
+	print(sprintf("%s/kineticEnergy-N=%d-T=%ds-D=%d-Log10.png", dirName, N, round(time(end)), diameter), "-dpngcairo", "-F:14")
 end
 
